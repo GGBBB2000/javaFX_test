d1e7984ec8a8ea0dfd3ce55beb9d224d3f4d6b31
equal-l2 <eng.equall2@gmail.com>
2018-12-21 15:51:56 +0900

    Add utility function for loading FXML

--- MvPController.java
+++ MvPController.java
@@ -1,17 +1,15 @@
-import javafx.scene.shape.Circle;
-import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-import javafx.scene.layout.Pane;
-import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.Scene;
-import javafx.fxml.FXML; //FXMLをコード内で利用するためのもの
-import javafx.event.ActionEvent;
-import javafx.animation.Timeline;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML; //FXMLをコード内で利用するためのもの
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 import javafx.util.Duration;
+
 public class MvPController{
     private double x;
     private double y;
@@ -27,108 +25,107 @@ public class MvPController{
     private Button button;
     @FXML
     void initialize(){
-	final Timeline tl = new Timeline(); //Timelineで敵を動かせるようにする
-	tl.setCycleCount(Animation.INDEFINITE);
-	tl.getKeyFrames().add(new KeyFrame(
-		    Duration.millis(100),
-		    ActionEvent -> {
-			double x = circle1X;
-			double y = circle1Y;
-			System.out.printf("x:%f, y:%f\n", x, y);
-			setCircle1Position(x, y);
-		    }
-		    ));
-	// tl.play();
-	javafx.application.Platform.runLater(() -> { 
-	    //setOnKeyPressedはSceneに実装しないといけないので
-	    //遅延させてから実行している
-	    Scene scene = circle.getScene();
-	    scene.setOnKeyPressed(e -> { //キーボード入力
-		System.out.println(e.getCode().toString());
-		double x = circle.getLayoutX();
-		double y = circle.getLayoutY();
-		String code = e.getCode().toString();
-		if(code.equals("RIGHT")){
-		    setCircle(circle, x + 5, y);
-		}else if(code.equals("LEFT")){
-		    setCircle(circle, x - 5, y);
-		}else if(code.equals("UP")){
-		    setCircle(circle, x, y - 5);
-		}else if(code.equals("DOWN")){
-		    setCircle(circle, x, y + 5);
-		}
+        final Timeline tl = new Timeline(); //Timelineで敵を動かせるようにする
+        tl.setCycleCount(Animation.INDEFINITE);
+        tl.getKeyFrames().add(new KeyFrame(
+                    Duration.millis(100),
+                    event -> {
+                        double x = circle1X;
+                        double y = circle1Y;
+                        System.out.printf("x:%f, y:%f\n", x, y);
+                        setCircle1Position(x, y);
+                    }
+                    ));
+        // tl.play();
 
-	    });
-	});
+        javafx.application.Platform.runLater(() -> {
+            //setOnKeyPressedはSceneに実装しないといけないので
+            //遅延させてから実行している
+            Scene scene = MvP.getCurrentScene();
+            scene.setOnKeyPressed(e -> { //キーボード入力
+                System.out.println(e.getCode().toString());
+                double x = circle.getLayoutX();
+                double y = circle.getLayoutY();
+                String code = e.getCode().toString();
+                if(code.equals("RIGHT")){
+                    setCircle(circle, x + 5, y);
+                }else if(code.equals("LEFT")){
+                    setCircle(circle, x - 5, y);
+                }else if(code.equals("UP")){
+                    setCircle(circle, x, y - 5);
+                }else if(code.equals("DOWN")){
+                    setCircle(circle, x, y + 5);
+                }
+            });
+        });
     }
+
     @FXML
-    void onMoved(MouseEvent e) { 
-	setCircle(circle, e.getX(), e.getY());
-	mouseX = e.getX();
-	mouseY = e.getY();
+    void onMoved(MouseEvent e) {
+        setCircle(circle, e.getX(), e.getY());
+        mouseX = e.getX();
+        mouseY = e.getY();
     }
 
     @FXML
     void onMClicked(MouseEvent e) {
-	System.out.println("hogehogehogehoge");
-	changeView();
+        System.out.println("hogehogehogehoge");
+        changeView();
     }
 
     void changeView() {
-	Stage s = (Stage) button.getScene().getWindow();
-	try {
-	    Scene myScene = new Scene(FXMLLoader.load(getClass().getResource("Test.fxml")));
-	    s.setScene(myScene);
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
+        try {
+            MvP.changeSceneByFXML("Test.fxml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     void keyAction(KeyEvent e){
-	System.out.println(e.getCode());
+        System.out.println(e.getCode());
     }
 
     void setCircle(Circle object, double x, double y){
-	double upper = 100;
-	double bottom = 100 + 200;
-	double left = 200;
-	double right = 200 + 200;
-	if(x >= left + 10 && x <= right - 10){ 
-	    if(y >= upper + 10 && y <= bottom - 10){
-		object.setLayoutY(y);
-	    }
-	    object.setLayoutX(x);
-	}else{
-	    if(y >= upper + 10 && y <= bottom - 10){
-		object.setLayoutY(y);
-	    }
-	}
+        double upper = 100;
+        double bottom = 100 + 200;
+        double left = 200;
+        double right = 200 + 200;
+        if(x >= left + 10 && x <= right - 10){
+            if(y >= upper + 10 && y <= bottom - 10){
+                object.setLayoutY(y);
+            }
+            object.setLayoutX(x);
+        }else{
+            if(y >= upper + 10 && y <= bottom - 10){
+                object.setLayoutY(y);
+            }
+        }
     }
 
     void setCircle1Position(double x, double y){
-	double circleX = mouseX;
-	double circleY = mouseY;
-	//System.out.printf("circleX:%f", circleX);
-	if(x > circleX){
-	    if(y > circleY){
-		setCircle(circle1, x, y + 5);
-		circle1Y += 5;
-	    }else{
-		setCircle(circle1, x, y - 5);
-		circle1Y -= 5;
-	    }
-	    setCircle(circle1, x + 5, y);
-	    circle1X += 5;
-	}else{
-	    if(y > circleY){
-		setCircle(circle1, x, y + 5);
-		circle1Y += 5;
-	    }else{
-		setCircle(circle1, x, y - 5);
-		circle1Y -= 5;
-	    }
-	    setCircle(circle1, x - 5, y);
-	    circle1X -= 5;
-	}
+        double circleX = mouseX;
+        double circleY = mouseY;
+        //System.out.printf("circleX:%f", circleX);
+        if(x > circleX){
+            if(y > circleY){
+                setCircle(circle1, x, y + 5);
+                circle1Y += 5;
+            }else{
+                setCircle(circle1, x, y - 5);
+                circle1Y -= 5;
+            }
+            setCircle(circle1, x + 5, y);
+            circle1X += 5;
+        }else{
+            if(y > circleY){
+                setCircle(circle1, x, y + 5);
+                circle1Y += 5;
+            }else{
+                setCircle(circle1, x, y - 5);
+                circle1Y -= 5;
+            }
+            setCircle(circle1, x - 5, y);
+            circle1X -= 5;
+        }
     }
 }
--- TestController.java
+++ TestController.java
@@ -1,15 +1,11 @@
-import javafx.scene.shape.Circle;
-import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-import javafx.scene.layout.Pane;
-import javafx.scene.Scene;
-import javafx.fxml.FXML; //FXMLをコード内で利用するためのもの
-import javafx.event.ActionEvent;
-import javafx.animation.Timeline;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
-import javafx.util.Duration;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML; //FXMLをコード内で利用するためのもの
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
 public class TestController{
     int count;
     Timeline t;
@@ -18,31 +14,29 @@ public class TestController{
 
     @FXML
     void initialize(){
-	count = 11;
-	/* タイマーの設定 */
-	t = new Timeline(
-		/* 1000 milli sec　間隔で実行 */
-		new KeyFrame(Duration.millis(1000),
-		    event -> {
-			time.setText(String.format("00:%02d", count));
-			if (count-- < 0) {
-			    Stage s = (Stage) time.getScene().getWindow();
-			    try {
-				/* Timelineを止めておかないと切り替え後にも
-				 * 動いてしまう*/
-				t.stop();
-				Scene scene = new Scene(FXMLLoader.load(getClass()
-					    .getResource("MvPController.fxml")));
-				s.setScene(scene);
-			    } catch (Exception e) {
-				e.printStackTrace();
-			    }
-			}
-		}
-		)
-		);
-	/* 何回アニメーションを実行するかの設定(ここでは無限ループ) */
-	t.setCycleCount(Animation.INDEFINITE);
-	t.play();
+        count = 11;
+        /* タイマーの設定 */
+        t = new Timeline(
+            /* 1000 milli sec　間隔で実行 */
+            new KeyFrame(
+                Duration.millis(1000),
+                event -> {
+                    time.setText(String.format("00:%02d", count));
+                    if (count-- < 0) {
+                        try {
+                            /* Timelineを止めておかないと切り替え後にも
+                             * 動いてしまう*/
+                            t.stop();
+                            MvP.changeSceneByFXML("MvPController.fxml");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            )
+        );
+        /* 何回アニメーションを実行するかの設定(ここでは無限ループ) */
+        t.setCycleCount(Animation.INDEFINITE);
+        t.play();
     }
 }
